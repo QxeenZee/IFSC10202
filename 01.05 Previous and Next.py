f4fd1748fbfcdc3160bed111a3cd0ea3d2d42cba
Zee <zqdotson@ualr.edu>
2026-01-29 20:38:10 +0000

    m

---
+++ 01.05 Previous and Next.py
@@ -0,0 +1,13 @@
+#Prompt user for a number
+number = int(input("Enter a number: "))
+
+#Calculate next number
+calculate_next_number = number + 1
+
+#Calculate previous number
+calculate_prev_number = number - 1
+
+
+#Print results
+print ("The next number for the number " + number " is " + calculate_next_number + "." )
+print ("The previous number for the number " + number " is " + calculate_prev_number + "." )
