041eeb9ef1e23b87eca943c792f2e43fbb7d10ca
Zee <zqdotson@ualr.edu>
2026-01-29 20:38:42 +0000

    m

--- 01.05 Previous and Next.py
+++ 01.05 Previous and Next.py
@@ -7,7 +7,6 @@ calculate_next_number = number + 1
 #Calculate previous number
 calculate_prev_number = number - 1
 
-
 #Print results
 print ("The next number for the number " + number " is " + calculate_next_number + "." )
 print ("The previous number for the number " + number " is " + calculate_prev_number + "." )
