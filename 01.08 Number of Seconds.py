0a8f1cd30cdb8f3530f6f59deb3ce6ed6e358c4f
Zee <zqdotson@ualr.edu>
2026-02-01 20:16:13 +0000

    update

---
+++ 01.08 Number of Seconds.py
@@ -0,0 +1,8 @@
+#Get the minutes and seconds
+minutes = int(input("Enter Minutes: "))
+seconds = int(input("Enter Seconds: "))
+
+#Calculate number of seconds
+calculate_seconds = (minutes * 60) + seconds
+
+print (calculate_seconds)
