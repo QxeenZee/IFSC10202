b4e50f7496ef79f8e702330f85b2914d002bbea7
Zee <zqdotson@ualr.edu>
2026-04-13 12:49:44 +0000

    update

--- 10.Project.py
+++ 10.Project.py
@@ -1,47 +1,47 @@
 # Step 1 - Create a class called "Student"
-
 class Student:
 
-# Step 2 - Define the initializer, the initializer should accept the folloing class values:
-	def __init__(self, firstname ,lastname ,tnumber, scores):
-          
-# Step 3 - In the initializer, create the following object attributes
-self.FirstName = firstname
-self.LastName = lastname
-self.TNumber = tnumber
-self.Grades = scores
+    # Step 2 - Define the initializer
+    def __init__(self, firstname, lastname, tnumber, scores):
 
-# Step 4 - Define the methods for the object
-#Calculates the running average (non-blank) of the scores
-def RunningAverage(self):
+        # Step 3 - Create attributes
+        self.FirstName = firstname
+        self.LastName = lastname
+        self.TNumber = tnumber
+        self.Grades = scores
+
+    # Calculates the running average (non-blank)
+    def RunningAverage(self):
         total = 0
         count = 0
 
         for grade in self.Grades:
             if grade != "":
-                total = total + float(grade)
-                count = count + 1
+                total += float(grade)
+                count += 1
 
         if count == 0:
             return 0
         else:
             return total / count
-#Calculates the average of the scores, where missing scores are treated a zero
-def TotalAverage(self):
+
+    # Calculates the semester average (blank = 0)
+    def TotalAverage(self):
         total = 0
 
         for grade in self.Grades:
             if grade == "":
-                total = total + 0
+                total += 0
             else:
-                total = total + float(grade)
+                total += float(grade)
 
         if len(self.Grades) == 0:
             return 0
         else:
             return total / len(self.Grades)
-#Returns the letter grade based on TotalAverage
-def LetterGrade(self):
+
+    # Returns letter grade
+    def LetterGrade(self):
         avg = self.TotalAverage()
 
         if avg >= 90:
@@ -54,7 +54,8 @@ def LetterGrade(self):
             return "D"
         else:
             return "F"
-            
+
+
 # HEADER
 print("       First         Last           ID      Running     Semester       Letter")
 print("        Name         Name       Number      Average      Average        Grade")
